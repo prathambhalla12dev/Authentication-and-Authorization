ae07003e0d92b220ef0eb4577ca3b3bb6f428e02
Pratham <astrotalk@Astrotalks-MacBook-Pro-7.local>
2026-09-24 18:09:11 +0530

    create users table

---
+++ resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id       BIGSERIAL    PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email    VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    country  VARCHAR(100) NOT NULL,
+    role     VARCHAR(10)  NOT NULL DEFAULT 'USER'
+);
